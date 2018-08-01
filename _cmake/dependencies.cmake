108f040ff3b112bafcfd87606e9d73f5edff4abe
Austin Gilbert <ceretullis@gmail.com>
2018-08-01 17:37:01 -0500

    Using conan for dependencies

--- _cmake/dependencies.cmake
+++ _cmake/dependencies.cmake
@@ -2,6 +2,3 @@
 include_directories(${CMAKE_SOURCE_DIR})
 include(_cmake/AddLibrary/Dependencies.cmake)
 
-# our project dependencies 
-include(_cmake/dependencies/Boost.cmake)
-include(_cmake/dependencies/UnitTest++.cmake)
